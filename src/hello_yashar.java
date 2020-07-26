7d753c40e75ae3aeafe26caebdf1f2f0624636a5
wyashar <68728316+wyashar@users.noreply.github.com>
2020-07-26 00:53:12 -0400

    addedd printout

--- src/hello_yashar.java
+++ src/hello_yashar.java
@@ -2,6 +2,8 @@ public class hello_yashar {
     public static void main(String [] args) {
         System.out.println("He");
         System.out.println("Hello yashar");
+        System.out.println("Hello");
+
 
     }
 }
